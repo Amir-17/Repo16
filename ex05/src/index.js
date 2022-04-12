451f1c5cb02efb6985b1693ac160459cc43bba3d
Amir-17 <amirkahriman17@gmail.com>
2022-04-12 13:57:31 +0200

    Repo16 done

---
+++ ex05/src/index.js
@@ -0,0 +1,32 @@
+class Microsoft {
+    constructor(name) {
+        this.name = name;
+    };
+    get name() {
+        return this._name
+    };
+    set name(newName) {
+        this._name = newName;
+    };
+    occupation() {
+        return `${this.name} is a philantrhopist!`
+    };
+};
+
+class Facebook extends Microsoft {
+    constructor(name, age) {
+        super(name);
+        this._age = age;
+    };
+    studentAge() {
+        return `${this._name} is ${this._age}`;
+    };
+};
+
+let henry = new Microsoft("William Henry Gates III");
+console.log(henry.occupation());
+
+let elliot = new Facebook("Mark Elliot Zuckerberg", 23);
+console.log(elliot.studentAge());
+
+module.exports = { Microsoft, Facebook };
---
+++ ex07/src/index.js
@@ -0,0 +1,3 @@
+const person = require('./nameFunction');
+
+console.log(`${person.firstName("John")} ${person.lastName("Doe")}`);
